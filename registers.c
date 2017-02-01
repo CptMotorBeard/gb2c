ef32908009b2bc141a01651bc923b9adae37e6e0
Antonio La Rocca <roccanator@gmail.com>
2017-02-01 13:43:01 -0500

    Adding a structure for registers

---
+++ registers.c
@@ -0,0 +1,47 @@
+// The idea for the structure of these registers comes from the open source Gameboy Emulator Cinoop
+
+struct registers {
+	struct {
+		union {
+			struct {
+				unsigned char f;
+				unsigned char a;
+			};
+			unsigned short af;
+		};
+	};
+	
+	struct {
+		union {
+			struct {
+				unsigned char c;
+				unsigned char b;
+			};
+			unsigned short bc;
+		};
+	};
+	
+	struct {
+		union {
+			struct {
+				unsigned char e;
+				unsigned char d;
+			};
+			unsigned short de;
+		};
+	};
+	
+	struct {
+		union {
+			struct {
+				unsigned char l;
+				unsigned char h;
+			};
+			unsigned short hl;
+		};
+	};
+	
+	unsigned short sp;
+	unsigned short pc;
+} extern registers;
+
