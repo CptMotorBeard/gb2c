c26854bbf5f36f8026b95ca2412a6a9ec5b454e7
Antonio <roccanator@gmail.com>
2017-02-10 18:03:28 -0500

    Had an issue with unix/windows CR LF and LF endings which caused problems and it is now all fixed, also moved things for easier access and added the compare to the test script

--- sprint1/test.bash
+++ sprint1/test.bash
@@ -4,3 +4,8 @@ gcc -Wall reader.c -o reader
 echo "running reader on test case"
 ./reader Tetris.gb > output.asm
 echo "run complete. Output found in output.asm"
+echo "compiling comparer"
+gcc -Wall compare.c -o compare
+echo "comparing our output to suggested output"
+./compare output.asm Tetris.asm
+echo "comparing done, output found in differences.txt"
