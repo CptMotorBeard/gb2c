1fbfc894e7408bc4d76e5f15766fb36a0afd1bd6
Antonio <roccanator@gmail.com>
2017-02-11 20:47:01 -0500

    Error fixing

--- opcodes.c
+++ opcodes.c
@@ -1,21 +1,24 @@
 #include "hardware.h"
 #include "opcodes.h"
+#include <stdio.h>
 
 // Helper functions for opcodes
 // 8-bit loads
-void LD(Register r, BYTE immediate, int type) {
-	// Use 0 for lo and 1 for hi
+void LD(Register* r, BYTE immediate, int type) {
+	// Use 1 for lo and 2 for hi
 	switch (type) {
-		case 0:
-			r.lo = immediate;
 		case 1:
-			r.hi = immediate;
+			r->lo = immediate;
+			break;
+		case 2:			
+			r->hi = immediate;
+			break;
 	}
 }
 
 // 16-bit loads
-void LD_16(Register r, WORD immediate) {
-	r.pair = immediate;
+void LD_16(Register* r, WORD immediate) {
+	r->pair = immediate;
 }
 
 // Flag operations
@@ -38,44 +41,44 @@ int flagSet(BYTE flag) {
 
 // Functions for all of the opcodes
 void NOP() {}
-void LD_BC(WORD operand) {LD_16(registerBC, operand);}
+void LD_BC(WORD operand) {LD_16(&registerBC, operand);}
 void LD_BC_A() {writeMemory(registerBC.pair, registerAF.hi);}
 void INC_BC() {}
 void INC_B() {}
 void DEC_B() {}
-void LD_B(BYTE operand) {LD(registerBC, operand, 1);}
+void LD_B(BYTE operand) {LD(&registerBC, operand, 2);}
 void RLCA() {}
-void LD_04X_SP(WORD operand) {LD_16(SP, operand);}
+void LD_04X_SP(WORD operand) {LD_16(&SP, operand);}
 void ADD_HL_BC() {}
-void LD_A_BC() {LD(registerAF, cpu[registerBC.pair], 1);}
+void LD_A_BC() {LD(&registerAF, cpu[registerBC.pair], 2);}
 void DEC_BC() {}
 void INC_C() {}
 void DEC_C() {}
-void LD_C(BYTE operand) {LD(registerBC, operand, 0);}
+void LD_C(BYTE operand) {LD(&registerBC, operand, 1);}
 void RRCA() {}
 void STOP(BYTE operand) {}
-void LD_DE(WORD operand) {LD_16(registerDE, operand);}
+void LD_DE(WORD operand) {LD_16(&registerDE, operand);}
 void LD_DE_A() {writeMemory(registerDE.pair, registerAF.hi);}
 void INC_DE() {}
 void INC_D() {}
 void DEC_D() {}
-void LD_D(BYTE operand) {LD(registerDE, operand, 1);}
+void LD_D(BYTE operand) {LD(&registerDE, operand, 2);}
 void RLA() {}
 void JR(BYTE operand) {JP(PC.pair + (SIGNED_BYTE)operand);}
 void ADD_HL_DE() {}
-void LD_A_DE() {LD(registerAF, cpu[registerDE.pair], 1);}
+void LD_A_DE() {LD(&registerAF, cpu[registerDE.pair], 2);}
 void DEC_DE() {}
 void INC_E() {}
 void DEC_E() {}
-void LD_E(BYTE operand) {LD(registerDE, operand, 0);}
+void LD_E(BYTE operand) {LD(&registerDE, operand, 1);}
 void RRA() {}
 void JR_NZ(BYTE operand) {if (!flagSet(flag_Z)){JR(operand);}}
-void LD_HL_WORD(WORD operand) {LD_16(registerHL, operand);}
+void LD_HL_WORD(WORD operand) {LD_16(&registerHL, operand);}
 void LDI_HL_A() {LD_HL_A; INC_HL;}
 void INC_HL() {}
 void INC_H() {}
 void DEC_H() {}
-void LD_H(BYTE operand) {LD(registerHL, operand, 1);}
+void LD_H(BYTE operand) {LD(&registerHL, operand, 2);}
 void DAA() {}
 void JR_Z(BYTE operand) {if (flagSet(flag_Z)){JR(operand);}}
 void ADD_HL_HL() {}
@@ -83,14 +86,14 @@ void LDI_A_HL() {LD_A_HL; INC_HL;}
 void DEC_HL() {}
 void INC_L() {}
 void DEC_L() {}
-void LD_L(BYTE operand) {LD(registerHL, operand, 0);}
+void LD_L(BYTE operand) {LD(&registerHL, operand, 1);}
 void CPL() {
 	setFlag(flag_N);
 	setFlag(flag_H);
 	registerAF.hi = ~registerAF.hi;
 }
 void JR_NC(BYTE operand) {if (!flagSet(flag_C)){JR(operand);}}
-void LD_SP(WORD operand) {LD_16(SP, operand);}
+void LD_SP(WORD operand) {LD_16(&SP, operand);}
 void LDD_HL_A() {LD_HL_A; DEC_HL;}
 void INC_SP() {}
 void LD_HL_BYTE(BYTE operand) {}
@@ -105,60 +108,60 @@ void LDD_A_HL() {LD_A_HL; DEC_HL;}
 void DEC_SP() {}
 void INC_A() {}
 void DEC_A() {}
-void LD_A_BYTE(BYTE operand) {LD(registerAF, operand, 1);}
+void LD_A_BYTE(BYTE operand) {LD(&registerAF, operand, 2);}
 void CCF() {
 	clearFlag(flag_N);
 	clearFlag(flag_H);
 	flipFlag(flag_C);
 }
-void LD_B_B() {LD(registerBC, registerBC.hi, 1);}
-void LD_B_C() {LD(registerBC, registerBC.lo, 1);}
-void LD_B_D() {LD(registerBC, registerDE.hi, 1);}
-void LD_B_E() {LD(registerBC, registerDE.lo, 1);}
-void LD_B_H() {LD(registerBC, registerHL.hi, 1);}
-void LD_B_L() {LD(registerBC, registerHL.lo, 1);}
-void LD_B_HL() {LD(registerBC, cpu[registerHL.pair], 1);}
-void LD_B_A() {LD(registerBC, registerAF.hi, 1);}
-void LD_C_B() {LD(registerBC, registerBC.hi, 0);}
-void LD_C_C() {LD(registerBC, registerBC.lo, 0);}
-void LD_C_D() {LD(registerBC, registerDE.hi, 0);}
-void LD_C_E() {LD(registerBC, registerDE.lo, 0);}
-void LD_C_H() {LD(registerBC, registerHL.hi, 0);}
-void LD_C_L() {LD(registerBC, registerHL.lo, 0);}
-void LD_C_HL() {LD(registerBC, cpu[registerHL.pair], 0);}
-void LD_C_A() {LD(registerBC, registerAF.hi, 0);}
-void LD_D_B() {LD(registerDE, registerBC.hi, 1);}
-void LD_D_C() {LD(registerDE, registerBC.lo, 1);}
-void LD_D_D() {LD(registerDE, registerDE.hi, 1);}
-void LD_D_E() {LD(registerDE, registerDE.lo, 1);}
-void LD_D_H() {LD(registerDE, registerHL.hi, 1);}
-void LD_D_L() {LD(registerDE, registerHL.lo, 1);}
-void LD_D_HL() {LD(registerDE, cpu[registerHL.pair], 1);}
-void LD_D_A() {LD(registerDE, registerAF.hi, 1);}
-void LD_E_B() {LD(registerDE, registerBC.hi, 0);}
-void LD_E_C() {LD(registerDE, registerBC.lo, 0);}
-void LD_E_D() {LD(registerDE, registerDE.hi, 0);}
-void LD_E_E() {LD(registerDE, registerDE.lo, 0);}
-void LD_E_H() {LD(registerDE, registerHL.hi, 0);}
-void LD_E_L() {LD(registerDE, registerHL.lo, 0);}
-void LD_E_HL() {LD(registerDE, cpu[registerHL.pair], 0);}
-void LD_E_A() {LD(registerDE, registerAF.hi, 0);}
-void LD_H_B() {LD(registerHL, registerBC.hi, 1);}
-void LD_H_C() {LD(registerHL, registerBC.lo, 1);}
-void LD_H_D() {LD(registerHL, registerDE.hi, 1);}
-void LD_H_E() {LD(registerHL, registerDE.lo, 1);}
-void LD_H_H() {LD(registerHL, registerHL.hi, 1);}
-void LD_H_L() {LD(registerHL, registerHL.lo, 1);}
-void LD_H_HL() {LD(registerHL, cpu[registerHL.pair], 1);}
-void LD_H_A() {LD(registerHL, registerAF.hi, 1);}
-void LD_L_B() {LD(registerHL, registerBC.hi, 0);}
-void LD_L_C() {LD(registerHL, registerBC.lo, 0);}
-void LD_L_D() {LD(registerHL, registerDE.hi, 0);}
-void LD_L_E() {LD(registerHL, registerDE.lo, 0);}
-void LD_L_H() {LD(registerHL, registerHL.hi, 0);}
-void LD_L_L() {LD(registerHL, registerHL.lo, 0);}
-void LD_L_HL() {LD(registerHL, cpu[registerHL.pair], 0);}
-void LD_L_A() {LD(registerHL, registerAF.hi, 0);}
+void LD_B_B() {LD(&registerBC, registerBC.hi, 2);}
+void LD_B_C() {LD(&registerBC, registerBC.lo, 2);}
+void LD_B_D() {LD(&registerBC, registerDE.hi, 2);}
+void LD_B_E() {LD(&registerBC, registerDE.lo, 2);}
+void LD_B_H() {LD(&registerBC, registerHL.hi, 2);}
+void LD_B_L() {LD(&registerBC, registerHL.lo, 2);}
+void LD_B_HL() {LD(&registerBC, cpu[registerHL.pair], 2);}
+void LD_B_A() {LD(&registerBC, registerAF.hi, 2);}
+void LD_C_B() {LD(&registerBC, registerBC.hi, 1);}
+void LD_C_C() {LD(&registerBC, registerBC.lo, 1);}
+void LD_C_D() {LD(&registerBC, registerDE.hi, 1);}
+void LD_C_E() {LD(&registerBC, registerDE.lo, 1);}
+void LD_C_H() {LD(&registerBC, registerHL.hi, 1);}
+void LD_C_L() {LD(&registerBC, registerHL.lo, 1);}
+void LD_C_HL() {LD(&registerBC, cpu[registerHL.pair], 1);}
+void LD_C_A() {LD(&registerBC, registerAF.hi, 1);}
+void LD_D_B() {LD(&registerDE, registerBC.hi, 2);}
+void LD_D_C() {LD(&registerDE, registerBC.lo, 2);}
+void LD_D_D() {LD(&registerDE, registerDE.hi, 2);}
+void LD_D_E() {LD(&registerDE, registerDE.lo, 2);}
+void LD_D_H() {LD(&registerDE, registerHL.hi, 2);}
+void LD_D_L() {LD(&registerDE, registerHL.lo, 2);}
+void LD_D_HL() {LD(&registerDE, cpu[registerHL.pair], 2);}
+void LD_D_A() {LD(&registerDE, registerAF.hi, 2);}
+void LD_E_B() {LD(&registerDE, registerBC.hi, 1);}
+void LD_E_C() {LD(&registerDE, registerBC.lo, 1);}
+void LD_E_D() {LD(&registerDE, registerDE.hi, 1);}
+void LD_E_E() {LD(&registerDE, registerDE.lo, 1);}
+void LD_E_H() {LD(&registerDE, registerHL.hi, 1);}
+void LD_E_L() {LD(&registerDE, registerHL.lo, 1);}
+void LD_E_HL() {LD(&registerDE, cpu[registerHL.pair], 1);}
+void LD_E_A() {LD(&registerDE, registerAF.hi, 1);}
+void LD_H_B() {LD(&registerHL, registerBC.hi, 2);}
+void LD_H_C() {LD(&registerHL, registerBC.lo, 2);}
+void LD_H_D() {LD(&registerHL, registerDE.hi, 2);}
+void LD_H_E() {LD(&registerHL, registerDE.lo, 2);}
+void LD_H_H() {LD(&registerHL, registerHL.hi, 2);}
+void LD_H_L() {LD(&registerHL, registerHL.lo, 2);}
+void LD_H_HL() {LD(&registerHL, cpu[registerHL.pair], 2);}
+void LD_H_A() {LD(&registerHL, registerAF.hi, 2);}
+void LD_L_B() {LD(&registerHL, registerBC.hi, 1);}
+void LD_L_C() {LD(&registerHL, registerBC.lo, 1);}
+void LD_L_D() {LD(&registerHL, registerDE.hi, 1);}
+void LD_L_E() {LD(&registerHL, registerDE.lo, 1);}
+void LD_L_H() {LD(&registerHL, registerHL.hi, 1);}
+void LD_L_L() {LD(&registerHL, registerHL.lo, 1);}
+void LD_L_HL() {LD(&registerHL, cpu[registerHL.pair], 1);}
+void LD_L_A() {LD(&registerHL, registerAF.hi, 1);}
 void LD_HL_B() {writeMemory(registerHL.pair, registerBC.hi);}
 void LD_HL_C() {writeMemory(registerHL.pair, registerBC.lo);}
 void LD_HL_D() {writeMemory(registerHL.pair, registerDE.hi);}
@@ -167,14 +170,14 @@ void LD_HL_H() {writeMemory(registerHL.pair, registerHL.hi);}
 void LD_HL_L() {writeMemory(registerHL.pair, registerHL.lo);}
 void HALT() {}
 void LD_HL_A() {writeMemory(registerHL.pair, registerAF.hi);}
-void LD_A_B() {LD(registerAF, registerBC.hi, 1);}
-void LD_A_C() {LD(registerAF, registerBC.lo, 1);}
-void LD_A_D() {LD(registerAF, registerDE.hi, 1);}
-void LD_A_E() {LD(registerAF, registerDE.lo, 1);}
-void LD_A_H() {LD(registerAF, registerHL.hi, 1);}
-void LD_A_L() {LD(registerAF, registerHL.lo, 1);}
-void LD_A_HL() {LD(registerAF, cpu[registerHL.pair], 1);}
-void LD_A_A() {LD(registerAF, registerAF.hi, 1);}
+void LD_A_B() {LD(&registerAF, registerBC.hi, 2);}
+void LD_A_C() {LD(&registerAF, registerBC.lo, 2);}
+void LD_A_D() {LD(&registerAF, registerDE.hi, 2);}
+void LD_A_E() {LD(&registerAF, registerDE.lo, 2);}
+void LD_A_H() {LD(&registerAF, registerHL.hi, 2);}
+void LD_A_L() {LD(&registerAF, registerHL.lo, 2);}
+void LD_A_HL() {LD(&registerAF, cpu[registerHL.pair], 2);}
+void LD_A_A() {LD(&registerAF, registerAF.hi, 2);}
 void ADD_A_B() {}
 void ADD_A_C() {}
 void ADD_A_D() {}
@@ -241,8 +244,8 @@ void CP_HL() {}
 void CP_A() {}
 void RET_NZ() {if (!flagSet(flag_Z)){RET();}}
 void POP_BC() {
-	LD_16(registerBC, cpu[SP.pair]);
-	SP.pair++;SP.pair++;
+	LD(&registerBC, cpu[SP.pair], 2);SP.pair++;
+	LD(&registerBC, cpu[SP.pair], 1);SP.pair++;
 }
 void JP_NZ(WORD operand) {if (!flagSet(flag_Z)){JP(operand);}}
 void JP(WORD operand) {PC.pair = operand;}
@@ -287,8 +290,8 @@ void RST_08() {
 }
 void RET_NC() {if (!flagSet(flag_C)){RET();}}
 void POP_DE() {
-	LD_16(registerDE, cpu[SP.pair]);
-	SP.pair++;SP.pair++;
+	LD(&registerDE, cpu[SP.pair], 2);SP.pair++;
+	LD(&registerDE, cpu[SP.pair], 1);SP.pair++;
 }
 void JP_NC(WORD operand) {if (!flagSet(flag_C)){JP(operand);}}
 void CALL_NC(WORD operand) {if (!flagSet(flag_C)){CALL(operand);}}
@@ -320,8 +323,8 @@ void RST_18() {
 }
 void LD_FF02X_A(BYTE operand) {writeMemory(0xFF00 + operand, registerAF.hi);}
 void POP_HL() {
-	LD_16(registerHL, cpu[SP.pair]);
-	SP.pair++;SP.pair++;
+	LD(&registerHL, cpu[SP.pair], 2);SP.pair++;
+	LD(&registerHL, cpu[SP.pair], 1);SP.pair++;
 }
 void LD_FFC_A() {writeMemory(0xFF00 + registerBC.lo, registerAF.hi);}
 void PUSH_HL() {
@@ -349,12 +352,12 @@ void RST_28() {
 	writeMemory(SP.pair, PC.hi);
 	JP(0x0028);
 }
-void LD_A_FF02X(BYTE operand) {LD(registerAF, cpu[0xFF00 + operand], 1);}
+void LD_A_FF02X(BYTE operand) {LD(&registerAF, cpu[0xFF00 + operand], 2);}
 void POP_AF() {
-	LD_16(registerAF, cpu[SP.pair]);
-	SP.pair++;SP.pair++;
+	LD(&registerAF, cpu[SP.pair], 2);SP.pair++;
+	LD(&registerAF, cpu[SP.pair], 1);SP.pair++;
 }
-void LD_A_FFC() {LD(registerAF, cpu[0xFF00 + registerBC.lo], 1);}
+void LD_A_FFC() {LD(&registerAF, cpu[0xFF00 + registerBC.lo], 2);}
 void DI() {}
 void PUSH_AF() {
 	SP.pair--;
@@ -378,10 +381,10 @@ void LD_HL_SP02X(BYTE operand) {
 	else {clearFlag(flag_C);}
 	if (((SP.pair & 0xF) + (operand & 0xF)) >> 0xF){setFlag(flag_H);}
 	else {clearFlag(flag_H);}
-	LD_16(registerHL, (WORD)(result & 0xFFFF));
+	LD_16(&registerHL, (WORD)(result & 0xFFFF));
 }
-void LD_SP_HL() {LD_16(SP, registerHL.pair);}
-void LD_A_WORD(WORD operand) {LD(registerAF, cpu[operand], 1);}
+void LD_SP_HL() {LD_16(&SP, registerHL.pair);}
+void LD_A_WORD(WORD operand) {LD(&registerAF, cpu[operand], 2);}
 void EI() {}
 void CP(BYTE operand) {}
 void RST_38() {
